cabe33b0916e88bfec77d9e0fd5116da234c4f4b
Nosdovah <nosdovah@LAPTOP-239E2HQR>
2023-09-02 18:40:10 +0700

    Alpha UI for RockPaperScissors

--- script.js
+++ script.js
@@ -1,74 +1,84 @@
-function getComputerChoice(arrChoice) {
-    let compChoice = ""
-    if(arrChoice.length <= 1) {
-        return "choice not valid"
+//Choosing for player choice based on keydown eventListener
+
+const button = document.querySelectorAll('.RPS_Div button')
+button.forEach(node => node.addEventListener(('click'), function(element) {
+    
+    //Define player choice 
+    let playerChoice = (element.target.classList.value)
+    
+    function getComputerChoice(arrChoice) {
+        let compChoice = ""
+        if(arrChoice.length <= 1) {
+            return "choice not valid"
+        }
+        compChoice = Math.floor(Math.random() * arrChoice.length)
+        return arrChoice[compChoice];
     }
 
-    compChoice = Math.floor(Math.random() * arrChoice.length)
-    return arrChoice[compChoice];
-}
+    function Result (playerChoice,compChoice,arrChoice) {
+        let result = ""
+        if( (playerChoice == null || playerChoice == undefined) || (compChoice == null || compChoice == undefined) || (arrChoice.includes(playerChoice) == false) ) {
+            return result = "invalid choice"
+        }else{
+                if(playerChoice == compChoice) {
+                    return result = "It's a Tie!"     
+                }
+                if(playerChoice == "Rock") {
+                    //Rock
+                    if(compChoice == "Scissors") {
+                        return result = `You Win! ${compChoice} beats ${playerChoice}`
+                    }else {
+                        return result = `You Lose! ${playerChoice} beats ${compChoice}`
+                    }
+                }else if(playerChoice == "Paper") {
+                    //Paper
+                    if(compChoice =="Scissors") {
+                        return result = `You Lose! ${compChoice} beats ${playerChoice}`
+                    }else {
+                        return result = `You Win! ${playerChoice} beats ${compChoice}`
+                    }
+                }else if(playerChoice == "Scissors") {
+                    if(compChoice == "Paper") {
+                        return result = `You Win! ${playerChoice} beats ${compChoice}`
+                    }else {
+                        return result = `You Lose! ${compChoice} beats ${playerChoice}`
+                    }
+                }
+        }
+        return result;
 
-function Result (playerChoice,compChoice,arrChoice) {
-    let result = ""
-    if( (playerChoice == null || playerChoice == undefined) || (compChoice == null || compChoice == undefined) || (arrChoice.includes(playerChoice) == false) ) {
-        return result = "invalid choice"
     }
-    //Rock
 
-    if(playerChoice == arrChoice[0]) {
-        if(playerChoice == arrChoice[0] && compChoice == arrChoice[0]) {
-            return result = "It's a Tie!"
-        }else if(playerChoice == arrChoice[0] && compChoice == arrChoice[1]) {
-            return result = `You Lose! ${compChoice} beats ${playerChoice}`
-        }else if(playerChoice == arrChoice[0] && compChoice == arrChoice[2]) {
-            return result = `You Win! ${playerChoice} beats ${compChoice}`
+    function playRound() {
+        let arrChoice = ["Rock","Paper","Scissors"]
+        const random = Math.floor(Math.random() * arrChoice.length);
+        let randdisplay = arrChoice[random]
+        
+        //Invoke Player choice function here
+        //let playerchoice = prompt("Please enter your choice",randdisplay)
+        let compchoice = getComputerChoice(arrChoice)
+
+        const result = Result(playerChoice,compchoice,arrChoice)
+
+        return result
+
+    }
+
+    
+    //Round of 5
+    let rounds = function (rounds) {
+        console.log(playRound())
+        let counter = 0;
+        while(counter < rounds) {
+            counter ++;
         }
     }
 
-    if(playerChoice == arrChoice[1]) {
-        if(playerChoice == arrChoice[1] && compChoice == arrChoice[1]) {
-            return result = "It's a Tie!"
-        }else if(playerChoice == arrChoice[1] && compChoice == arrChoice[2]) {
-            return result = `You Lose! ${compChoice} beats ${playerChoice}`
-        }else if(playerChoice == arrChoice[1] && compChoice == arrChoice[0]) {
-            return result = `You Win! ${playerChoice} beats ${compChoice}`
-        }
-    }
 
-    if(playerChoice == arrChoice[2]) {
-        if(playerChoice == arrChoice[2] && compChoice == arrChoice[2]) {
-            return result = "It's a Tie!"
-        }else if(playerChoice == arrChoice[2] && compChoice == arrChoice[0]) {
-            return result = `You Lose! ${compChoice} beats ${playerChoice}`
-        }else if(playerChoice == arrChoice[2] && compChoice == arrChoice[1]) {
-            return result = `You Win! ${playerChoice} beats ${compChoice}`
-        }
-    }
+}))
 
-    return result;
 
-}
 
-function playRound() {
-    let arrChoice = ["Rock","Paper","Scissors"]
-    const random = Math.floor(Math.random() * arrChoice.length);
-    let randdisplay = arrChoice[random]
 
-    let playerchoice = prompt("Please enter your choice",randdisplay)
-    let compchoice = getComputerChoice(arrChoice)
 
-    const result = Result(playerchoice,compchoice,arrChoice)
-
-    return result
-
-}
-
-function game(rounds) {
-    for (let index = 0; index < rounds; index++) {
-        console.log(playRound());
-    }
-}
-
-let rounds = prompt("How many rounds of RPS do you wanna play ?",1)
-
-game(rounds)
+// let rounds = prompt("How many rounds of RPS do you wanna play ?",1)
